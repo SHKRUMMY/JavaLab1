3634c68d95685367f63cffa753f8ea5dcbc06f8c
Yulian Shkrum <yulianshkrum@gmail.com>
2022-02-21 15:53:34 +0200

    added package with Main.java and PrinterRecord.java files. Filled the README file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Lab
+Lab N1. Variant 14. 
+
+Task to create Printer record class with 6 fields (3 are private), static field and method, that returns that static field and to override totring() method.
+Package includes Main.java and PrinterRecord.java files.
+To compile the programm: javac Path\To\The\Programm\Main.java in cmd prompt (Path includes package)
+To run the programm: java Path\To\The\Programm\Main.java in cmd prompt (Path includes package)
---
+++ com/iot/lpnu/PrinterRecord.java
@@ -0,0 +1,29 @@
+package com.iot.lpnu;
+
+public record PrinterRecord(String name, float effectvity, float price, String color, String model,
+		int productionDate) {
+
+	
+
+	public PrinterRecord() {	// Default constructor
+		this("Cannon", 60f, 130f);	// 3 set fields. The beginning of the constructor chain. 
+	}
+
+	public PrinterRecord(String name, float effectivity, float price) {	// Constructor with 3 parameters
+		this(name, effectivity, price, "black", "M3", 2020);	// Only 3 first field given, letter are set.
+		System.out.println("Consturctor with 3 fields was calledd");	//Logging the constructor calls
+		
+	}
+
+	private static int VOLTAGE = 220;	// Static field, spelled in Upper case.
+
+	public static int getStatic() {		//Static method, which returns static field
+		return VOLTAGE;
+	}
+
+	@Override	//Redefinition of toString() method with better visual clarity
+	public String toString() {
+		return "\n\nPrinter\nName:\tEffectivity:\tPrice:\tColor:\tModel:\tProduction Date\n" + name + "\t" + effectvity + "\t\t" +
+	price + "\t"+ color+ "\t"  + model + "\t"+ productionDate;
+	}
+}
